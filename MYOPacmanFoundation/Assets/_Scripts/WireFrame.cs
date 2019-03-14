ec09224fe36e6a29ea1dc4969f16133c1b9dacb4
NiallD565 <G00338932@gmit.ie>
2019-03-14 16:26:42 +0000

    Scale fixes

--- MYOPacmanFoundation/Assets/_Scripts/WireFrame.cs
+++ MYOPacmanFoundation/Assets/_Scripts/WireFrame.cs
@@ -7,6 +7,6 @@ public class WireFrame : MonoBehaviour {
     // draw a circle when the spawnpoint is drawn at design time
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(transform.position, 0.02f);
+        Gizmos.DrawWireSphere(transform.position, 0.07f);
     }
 }
