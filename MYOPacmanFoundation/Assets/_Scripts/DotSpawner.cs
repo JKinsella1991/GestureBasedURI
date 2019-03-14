84c64684fc62dbff736dfef63af89021d3f85c3a
NiallD565 <G00338932@gmit.ie>
2019-03-14 12:25:26 +0000

    maze colliders

---
+++ MYOPacmanFoundation/Assets/_Scripts/DotSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotSpawner : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject DotsPrefab;
+
+    // Use this for initialization
+    void Start () {
+        GameObject smallDots = Instantiate(DotsPrefab, transform.position, transform.rotation);
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ MYOPacmanFoundation/Assets/_Scripts/WireFrame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireFrame : MonoBehaviour {
+
+    // draw a circle when the spawnpoint is drawn at design time
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 0.05f);
+    }
+}
