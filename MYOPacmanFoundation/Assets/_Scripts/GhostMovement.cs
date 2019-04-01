f3bbe0aa168a9658e118b969e9938e288be05582
NiallD565 <G00338932@gmit.ie>
2019-04-01 16:20:16 +0100

    Increase scale

--- MYOPacmanFoundation/Assets/_Scripts/GhostMovement.cs
+++ MYOPacmanFoundation/Assets/_Scripts/GhostMovement.cs
@@ -18,33 +18,24 @@ public class GhostMovement : MonoBehaviour
 
     void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position,
-                            waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
 
         if (transform.position == waypoints [waypointIndex].transform.position)
         {
-            waypointIndex += 1;
+            IncrementIndex();
         }
 
         if (waypointIndex == waypoints.Length)
             waypointIndex = 0;
 
-        /*
-        if (transform.position != waypoints[cur].position)
-        {
-            Vector2 p = Vector2.MoveTowards(transform.position,
-                                            waypoints[cur].position,
-                                            speed);
-            GetComponent<Rigidbody2D>().MovePosition(p);
-        }
-        // Waypoint reached, select next one
-        else cur = (cur + 1) % waypoints.Length;
+       
+    }
 
-        // Animation
-        Vector2 dir = waypoints[cur].position - transform.position;
-        GetComponent<Animator>().SetFloat("DirX", dir.x);
-        GetComponent<Animator>().SetFloat("DirY", dir.y);
-        */
+    void IncrementIndex()
+    {
+        Debug.Log(waypointIndex);
+        waypointIndex += 1;
     }
 
     void OnTriggerEnter2D(Collider2D co)
