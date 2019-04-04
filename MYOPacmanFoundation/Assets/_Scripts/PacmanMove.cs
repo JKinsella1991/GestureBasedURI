58f92d1903759e9253a103621095cd91b1e70307
NiallD565 <G00338932@gmit.ie>
2019-04-04 23:00:23 +0100

    Pacman gesture controls

--- MYOPacmanFoundation/Assets/_Scripts/PacmanMove.cs
+++ MYOPacmanFoundation/Assets/_Scripts/PacmanMove.cs
@@ -16,12 +16,13 @@ public class PacmanMove : MonoBehaviour
     private GameObject myoGameObject;
     private Pose _lastPose = Pose.Unknown;
 
-    ThalmicMyo thalmicMyo;
 
 
     void Start()
     {
         dest = transform.position;
+        myoGameObject = GameObject.FindGameObjectWithTag("Myo");
+
     }
 
     void FixedUpdate()
@@ -34,6 +35,9 @@ public class PacmanMove : MonoBehaviour
         Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
         GetComponent<Rigidbody2D>().MovePosition(p);
 
+        ThalmicMyo thalmicMyo = myoGameObject.GetComponent<ThalmicMyo>();
+
+
         // Check for Input if not moving
         if ((Vector2)transform.position == dest)
             if (thalmicMyo.pose != _lastPose)
@@ -44,30 +48,42 @@ public class PacmanMove : MonoBehaviour
                
                 if (thalmicMyo.pose == Pose.WaveOut)
                 {
-                    thalmicMyo.Vibrate(VibrationType.Medium);
+                    thalmicMyo.Vibrate(VibrationType.Short);
                     new InputSimulator().Keyboard.KeyPress(VirtualKeyCode.RIGHT);
                     ExtendUnlockAndNotifyUserAction(thalmicMyo);
                 }
                 if (thalmicMyo.pose == Pose.WaveIn)
                 {
-                    thalmicMyo.Vibrate(VibrationType.Medium);
+                    thalmicMyo.Vibrate(VibrationType.Short);
                     new InputSimulator().Keyboard.KeyPress(VirtualKeyCode.LEFT);
                     ExtendUnlockAndNotifyUserAction(thalmicMyo);
                 }
 
                 if (thalmicMyo.pose == Pose.FingersSpread)
                 {
-                    thalmicMyo.Vibrate(VibrationType.Medium);
+                    thalmicMyo.Vibrate(VibrationType.Short);
                     new InputSimulator().Keyboard.KeyPress(VirtualKeyCode.DOWN);
                     ExtendUnlockAndNotifyUserAction(thalmicMyo);
                 }
                 if (thalmicMyo.pose == Pose.Fist)
                 {
-                    thalmicMyo.Vibrate(VibrationType.Medium);
+                    thalmicMyo.Vibrate(VibrationType.Short);
                     new InputSimulator().Keyboard.KeyPress(VirtualKeyCode.UP);
                     ExtendUnlockAndNotifyUserAction(thalmicMyo);
                 }
             }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            dest = (Vector2)transform.position + Vector2.up;
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        if (thalmicMyo.pose == Pose.WaveOut)
+            dest = (Vector2)transform.position + Vector2.right;
+        if (Input.GetKey(KeyCode.DownArrow))
+            dest = (Vector2)transform.position - Vector2.up;
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        if (thalmicMyo.pose == Pose.WaveIn)
+            dest = (Vector2)transform.position - Vector2.right;
 
         // Animation Parameters
         Vector2 dir = dest - (Vector2)transform.position;
